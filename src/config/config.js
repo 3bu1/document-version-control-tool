ad5715e9613e8de933d88684e79489104a81bcdf
tribhuvan <tribhuvan@intelgorithm.com>
2020-09-09 22:25:15 +0530

    added app url

--- src/config/config.js
+++ src/config/config.js
@@ -9,6 +9,7 @@ const envVarsSchema = Joi.object()
     NODE_ENV: Joi.string().valid('production', 'development', 'test').required(),
     PORT: Joi.number().default(3000),
     MONGODB_URL: Joi.string().required().description('Mongo DB url'),
+    URL: Joi.string().required().description('app url'),
     JWT_SECRET: Joi.string().required().description('JWT secret key'),
     JWT_ACCESS_EXPIRATION_MINUTES: Joi.number().default(30).description('minutes after which access tokens expire'),
     JWT_REFRESH_EXPIRATION_DAYS: Joi.number().default(30).description('days after which refresh tokens expire'),
@@ -29,6 +30,7 @@ if (error) {
 module.exports = {
   env: envVars.NODE_ENV,
   port: envVars.PORT,
+  url: envVars.URL,
   mongoose: {
     url: envVars.MONGODB_URL + (envVars.NODE_ENV === '' ? '-test' : ''),
     options: {
--- src/docs/swaggerDef.js
+++ src/docs/swaggerDef.js
@@ -4,16 +4,16 @@ const config = require('../config/config');
 const swaggerDef = {
   openapi: '3.0.0',
   info: {
-    title: 'node-express-mongoose-boilerplate API documentation',
+    title: 'document-revision-control-tool API documentation',
     version,
     license: {
       name: 'MIT',
-      url: 'https://github.com/hagopj13/node-express-mongoose-boilerplate/blob/master/LICENSE',
+      url: '',
     },
   },
   servers: [
     {
-      url: `http://localhost:${config.port}/v1`,
+      url: `http://${config.url}:${config.port}/v1`,
     },
   ],
 };
