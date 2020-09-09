8753773f381486cc000d915ec6755221b6dd6109
tribhuvan <tribhuvan@intelgorithm.com>
2020-09-09 22:30:10 +0530

    added app url

--- src/config/config.js
+++ src/config/config.js
@@ -1,27 +1,37 @@
-const dotenv = require('dotenv');
-const path = require('path');
-const Joi = require('@hapi/joi');
+const dotenv = require("dotenv");
+const path = require("path");
+const Joi = require("@hapi/joi");
 
-dotenv.config({ path: path.join(__dirname, '../../.env') });
+dotenv.config({ path: path.join(__dirname, "../../.env") });
 
 const envVarsSchema = Joi.object()
   .keys({
-    NODE_ENV: Joi.string().valid('production', 'development', 'test').required(),
+    NODE_ENV: Joi.string()
+      .valid("production", "development", "test")
+      .required(),
     PORT: Joi.number().default(3000),
-    MONGODB_URL: Joi.string().required().description('Mongo DB url'),
-    URL: Joi.string().required().description('app url'),
-    JWT_SECRET: Joi.string().required().description('JWT secret key'),
-    JWT_ACCESS_EXPIRATION_MINUTES: Joi.number().default(30).description('minutes after which access tokens expire'),
-    JWT_REFRESH_EXPIRATION_DAYS: Joi.number().default(30).description('days after which refresh tokens expire'),
-    SMTP_HOST: Joi.string().description('server that will send the emails'),
-    SMTP_PORT: Joi.number().description('port to connect to the email server'),
-    SMTP_USERNAME: Joi.string().description('username for email server'),
-    SMTP_PASSWORD: Joi.string().description('password for email server'),
-    EMAIL_FROM: Joi.string().description('the from field in the emails sent by the app'),
+    MONGODB_URL: Joi.string().required().description("Mongo DB url"),
+    URL: Joi.string().required().description("app url"),
+    JWT_SECRET: Joi.string().required().description("JWT secret key"),
+    JWT_ACCESS_EXPIRATION_MINUTES: Joi.number()
+      .default(30)
+      .description("minutes after which access tokens expire"),
+    JWT_REFRESH_EXPIRATION_DAYS: Joi.number()
+      .default(30)
+      .description("days after which refresh tokens expire"),
+    SMTP_HOST: Joi.string().description("server that will send the emails"),
+    SMTP_PORT: Joi.number().description("port to connect to the email server"),
+    SMTP_USERNAME: Joi.string().description("username for email server"),
+    SMTP_PASSWORD: Joi.string().description("password for email server"),
+    EMAIL_FROM: Joi.string().description(
+      "the from field in the emails sent by the app"
+    ),
   })
   .unknown();
 
-const { value: envVars, error } = envVarsSchema.prefs({ errors: { label: 'key' } }).validate(process.env);
+const { value: envVars, error } = envVarsSchema
+  .prefs({ errors: { label: "key" } })
+  .validate(process.env);
 
 if (error) {
   throw new Error(`Config validation error: ${error.message}`);
@@ -30,9 +40,9 @@ if (error) {
 module.exports = {
   env: envVars.NODE_ENV,
   port: envVars.PORT,
-  url: envVars.URL,
+  url: envVars.NODE_ENV === "development" ? "localhost" : envVars.URL,
   mongoose: {
-    url: envVars.MONGODB_URL + (envVars.NODE_ENV === '' ? '-test' : ''),
+    url: envVars.MONGODB_URL + (envVars.NODE_ENV === "" ? "-test" : ""),
     options: {
       useCreateIndex: true,
       useNewUrlParser: true,
